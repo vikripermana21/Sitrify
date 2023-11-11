759cd044d869aa5b9c20c23e11702a2a4e4bd656
salmaep <salma.edyna.tif421@polban.ac.id>
2023-11-11 15:45:29 +0700

    add template frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navigate = useNavigate();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen w-screen flex">
+      <div className="w-7/12 flex flex-col gap-3 items-center p-5 content-center">
+        <div className="w-full ml-0 items-start flex content-start">
+          <img src="/image/logo.svg" alt="" className="" />
+        </div>
+
+        <div className="h-full flex justify-center items-center mb-28">
+          <img src="/image/Saly-10.png" alt="" className="" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="w-5/12 flex flex-col gap-3 items-center">
+        <div className="h-full flex justify-center items-center">
+          <div className="card max-w-md w-screen shadow-2xl bg-base-100">
+            <form className="card-body p-0 mt-7 mb-7">
+              <p className="text-left">Welcome!</p>
+              <p className="text-left font-bold text-3xl pt-5 pb-5">Login</p>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Username</span>
+                </label>
+                <input
+                  type="username"
+                  placeholder="Masukan username"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Masukan password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button
+                  onClick={() => navigate(`/prediksi`)}
+                  className="btn btn-primary"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/daftarArtisPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SideBar from "../components/sidebar";
+
+const DaftarArtisPage = () => {
+  return (
+    <div className="h-screen w-screen flex">
+      <div className="w-1/4 flex flex-col gap-3 items-center">
+        <SideBar />
+      </div>
+      <div className="w-3/4 flex flex-col gap-3 items-center p-5">
+        <div className="mt-24 w-3/4 justify-center items-center">
+          <div className="mb-5 "></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DaftarArtisPage;
