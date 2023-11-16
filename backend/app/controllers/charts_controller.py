9c500ed4cec8c745db86ecb0674c11e10e54a87d
Al Ghani <alghani63@gmail.com>
2023-11-16 19:11:15 +0700

    update chart controller

--- backend/app/controllers/charts_controller.py
+++ backend/app/controllers/charts_controller.py
@@ -1,2 +1,139 @@
+from flask_pymongo import PyMongo
+from app import app
+from flask import jsonify, request
+from datetime import datetime, timedelta
 
+mongo = PyMongo(app)
+
+def get_all_chart_data():
+    chart_collection = mongo.db.charts
+    chart_data = list(chart_collection.find({}, {'_id': 0}))  # Menggunakan list() untuk mengubah kursor menjadi daftar
+    return {"charts": chart_data}
     
+# def get_chart_by_timestamp(timestamp):
+#     collection = mongo.db.charts
+#     chart_data_by_timestamp = collection.find_one({'timestamp': timestamp})
+
+#     # Konversi ObjectId menjadi string
+#     if chart_data_by_timestamp and '_id' in chart_data_by_timestamp:
+#         chart_data_by_timestamp['_id'] = str(chart_data_by_timestamp['_id'])
+
+#     return chart_data_by_timestamp
+
+def get_chart_by_timestamp(timestamp):
+    collection = mongo.db.charts
+    chart_data_by_timestamp = collection.find_one({'timestamp': timestamp})
+
+    # Ambil informasi perubahan ranking menggunakan fungsi get_ranking_difference
+    day_before = (datetime.strptime(timestamp, "%Y-%m-%d") - timedelta(days=1)).strftime("%Y-%m-%d")
+    chart_yesterday = collection.find_one({'timestamp': day_before})
+
+    # Konversi ObjectId menjadi string
+    if chart_data_by_timestamp and '_id' in chart_data_by_timestamp:
+        chart_data_by_timestamp['_id'] = str(chart_data_by_timestamp['_id'])
+
+    # Modifikasi chart_songs untuk tidak menyertakan audio_features
+    if 'chart_songs' in chart_data_by_timestamp:
+        for song_today in chart_data_by_timestamp['chart_songs']:
+            song_id = song_today.get("id_song")
+            ranking_today = int(song_today.get("ranking"))
+
+            # Periksa apakah chart_yesterday tidak None sebelum mencoba mengakses chart_songs
+            if chart_yesterday and 'chart_songs' in chart_yesterday:
+                # Temukan chart kemarin dengan song_id yang sama
+                song_yesterday = next((song for song in chart_yesterday['chart_songs'] if song.get("id_song") == song_id), None)
+
+                if song_yesterday:
+                    ranking_yesterday = int(song_yesterday.get("ranking"))
+                    difference = ranking_yesterday - ranking_today
+                else:
+                    # Jika id_song tidak ditemukan pada chart kemarin, beri nilai "new"
+                    difference = "new"
+            else:
+                # Jika chart_yesterday None, beri nilai "new"
+                difference = "new"
+
+            # Tambahkan informasi selisih ke dalam dictionary song_today
+            song_today['difference'] = difference
+
+            # Hapus audio_features jika ada
+            if 'audio_features' in song_today:
+                del song_today['audio_features']
+
+    return chart_data_by_timestamp
+
+
+def get_ranking_difference(day, yesterday):
+    collection = mongo.db.charts
+
+    # Ambil data chart untuk suatu harihb ip p
+    chart_day = collection.find_one({'timestamp': day})
+    chart_yesterday = collection.find_one({'timestamp': yesterday})
+
+    # Jika data chart tidak ditemukan, kembalikan respons 404 Not Found
+    if not chart_day or not chart_yesterday:
+        return jsonify({"error": "Data not found"}), 404
+
+    # Buat dictionary untuk menyimpan selisih ranking setiap song
+    ranking_difference = {}
+
+    # Iterasi setiap chart pada hari ini dan cari chart pada kemarin
+    for song_today in chart_day.get("chart_songs", []):
+        song_id = song_today.get("id_song")
+        ranking_today = int(song_today.get("ranking"))
+
+        # Temukan chart kemarin dengan song_id yang sama
+        song_yesterday = next((song for song in chart_yesterday.get("chart_songs", []) if song.get("id_song") == song_id), None)
+
+        if song_yesterday:
+            ranking_yesterday = int(song_yesterday.get("ranking"))
+            difference = ranking_yesterday - ranking_today
+        else:
+            # Jika id_song tidak ditemukan pada chart kemarin, beri nilai "new"
+            difference = "new"
+
+        ranking_difference[song_id] = difference
+
+    return jsonify(ranking_difference), 200
+
+
+### CREATE CHART KALO BUTUH ###
+# def create_chart():
+#     data = request.get_json()
+#     collection = mongo.db.charts
+
+#     # Membuat chart baru dengan data dari request
+#     new_chart = {
+#         'ranking': data['ranking'],
+#         'artist': [{
+#             'id': artist['id'],
+#             'name': artist['name']
+#         } for artist in data['artist']],
+#         'id_song': data['id_song'],
+#         'song_name': data['song_name'],
+#         'image': data['image'],
+#         'audio_features': {
+#             'acousticness': data['audio_features']['acousticness'],
+#             'danceability': data['audio_features']['danceability'],
+#             'energy': data['audio_features']['energy'],
+#             'instrumentalness': data['audio_features']['instrumentalness'],
+#             'liveness': data['audio_features']['liveness'],
+#             'loudness': data['audio_features']['loudness'],
+#             'valence': data['audio_features']['valence']
+#         }
+#     }
+
+#     # Menyimpan chart ke dalam basis data
+#     result = collection.insert_one(new_chart)
+
+#     # Mengembalikan ID chart yang baru dibuat
+#     new_chart_id = str(result.inserted_id)
+
+#     # Membaca chart yang baru dibuat dari basis data
+#     created_chart = collection.find_one({'_id': ObjectId(new_chart_id)})
+
+#     # Konversi ObjectId menjadi string
+#     if created_chart and '_id' in created_chart:
+#         created_chart['_id'] = str(created_chart['_id'])
+
+#     return jsonify(created_chart), 201
--- backend/app/routes/charts_route.py
+++ backend/app/routes/charts_route.py
@@ -1,57 +1,91 @@
+# from flask import Blueprint, jsonify, request
+# from your_flask_app import mongo
+# from bson import ObjectId  # Import ObjectId for working with MongoDB IDs
+
+# charts_routes_blueprint = Blueprint('charts_routes', __name__)
+
+# @charts_routes_blueprint.route('/charts', methods=['GET'])
+# def get_all_chart_data():
+#     chart_collection = mongo.db.charts
+#     chart_data = chart_collection.find()
+
+#     if chart_data:
+#         return jsonify(chart_data)
+#     else:
+#         return jsonify({'error': 'Chart data not found'}), 404
+
+# @charts_routes_blueprint.route('/charts/<timestamp>', methods=['GET'])
+# def get_chart_by_timestamp(timestamp):
+#     chart_collection = mongo.db.charts
+#     chart_data = chart_collection.find_one({'timestamp': timestamp})
+
+#     if chart_data:
+#         return jsonify(chart_data)
+#     else:
+#         return jsonify({'error': 'Chart data not found'}), 404
+
+# @charts_routes_blueprint.route('/charts', methods=['POST'])
+# def add_chart():
+#     chart_collection = mongo.db.charts
+#     new_chart_data = request.get_json()
+
+#     # Assuming the request JSON includes at least a 'timestamp' field
+#     chart_id = chart_collection.insert_one(new_chart_data).inserted_id
+
+#     return jsonify({'message': 'Chart added successfully', 'chart_id': str(chart_id)})
+
+# @charts_routes_blueprint.route('/charts/<chart_id>', methods=['PUT'])
+# def update_chart(chart_id):
+#     chart_collection = mongo.db.charts
+#     updated_chart_data = request.get_json()
+
+#     result = chart_collection.update_one({'_id': ObjectId(chart_id)}, {'$set': updated_chart_data})
+
+#     if result.modified_count > 0:
+#         return jsonify({'message': 'Chart updated successfully'})
+#     else:
+#         return jsonify({'error': 'Chart not found or no changes were made'}), 404
+
+# @charts_routes_blueprint.route('/charts/<chart_id>', methods=['DELETE'])
+# def delete_chart(chart_id):
+#     chart_collection = mongo.db.charts
+#     result = chart_collection.delete_one({'_id': ObjectId(chart_id)})
+
+#     if result.deleted_count > 0:
+#         return jsonify({'message': 'Chart deleted successfully'})
+#     else:
+#         return jsonify({'error': 'Chart not found'}), 404
+
 from flask import Blueprint, jsonify, request
-from your_flask_app import mongo
-from bson import ObjectId  # Import ObjectId for working with MongoDB IDs
+from datetime import datetime, timedelta
+from app.controllers.charts_controller import get_all_chart_data, get_chart_by_timestamp, get_ranking_difference
 
-charts_routes_blueprint = Blueprint('charts_routes', __name__)
+charts_route = Blueprint('charts_route', __name__, url_prefix='/api')
 
-@charts_routes_blueprint.route('/charts', methods=['GET'])
-def get_all_chart_data():
-    chart_collection = mongo.db.charts
-    chart_data = chart_collection.find()
+@charts_route.route('/chart', methods=['GET'])
+def get_users():
+    users = get_all_chart_data()
+    return jsonify(users)
 
-    if chart_data:
-        return jsonify(chart_data)
+@charts_route.route('/chart/<timestamp>', methods=['GET'])
+def get_chart(timestamp):
+    chart = get_chart_by_timestamp(timestamp)
+    if chart:
+        return jsonify(chart)
     else:
-        return jsonify({'error': 'Chart data not found'}), 404
+        return jsonify({'message': 'Chart not found'}), 404
 
-@charts_routes_blueprint.route('/charts/<timestamp>', methods=['GET'])
-def get_chart_by_timestamp(timestamp):
-    chart_collection = mongo.db.charts
-    chart_data = chart_collection.find_one({'timestamp': timestamp})
+# Rute untuk mendapatkan selisih ranking setiap song antara suatu hari dan kemarin
+@charts_route.route('/chart/ranking-difference/<day>', methods=['GET'])
+def get_ranking_difference_route(day):
+    # Misalnya, 'day' dalam format 'YYYY-MM-DD'
+    # Ubah 'day' menjadi objek datetime
+    day_datetime = datetime.strptime(day, '%Y-%m-%d')
 
-    if chart_data:
-        return jsonify(chart_data)
-    else:
-        return jsonify({'error': 'Chart data not found'}), 404
+    # Hitung hari kemarin
+    yesterday_datetime = day_datetime - timedelta(days=1)
 
-@charts_routes_blueprint.route('/charts', methods=['POST'])
-def add_chart():
-    chart_collection = mongo.db.charts
-    new_chart_data = request.get_json()
+    # Ubah hari kemarin menjadi format string yang diharapkan
+    yesterday = yesterday_datetime.strftime('%Y-%m-%d')
 
-    # Assuming the request JSON includes at least a 'timestamp' field
-    chart_id = chart_collection.insert_one(new_chart_data).inserted_id
-
-    return jsonify({'message': 'Chart added successfully', 'chart_id': str(chart_id)})
-
-@charts_routes_blueprint.route('/charts/<chart_id>', methods=['PUT'])
-def update_chart(chart_id):
-    chart_collection = mongo.db.charts
-    updated_chart_data = request.get_json()
-
-    result = chart_collection.update_one({'_id': ObjectId(chart_id)}, {'$set': updated_chart_data})
-
-    if result.modified_count > 0:
-        return jsonify({'message': 'Chart updated successfully'})
-    else:
-        return jsonify({'error': 'Chart not found or no changes were made'}), 404
-
-@charts_routes_blueprint.route('/charts/<chart_id>', methods=['DELETE'])
-def delete_chart(chart_id):
-    chart_collection = mongo.db.charts
-    result = chart_collection.delete_one({'_id': ObjectId(chart_id)})
-
-    if result.deleted_count > 0:
-        return jsonify({'message': 'Chart deleted successfully'})
-    else:
-        return jsonify({'error': 'Chart not found'}), 404
+    return get_ranking_difference(day, yesterday)
