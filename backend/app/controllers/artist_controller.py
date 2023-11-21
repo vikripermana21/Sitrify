ef049df288ab9421f0dffcf8540c391a2d345e6c
mdyfann <dfnrmdhn02@gmail.com>
2023-11-21 17:52:13 +0700

    Add Controller dan Route Artist

---
+++ backend/app/controllers/artist_controller.py
@@ -0,0 +1,123 @@
+from flask_pymongo import PyMongo
+from app import app
+from flask import jsonify, request
+from bson import ObjectId
+
+mongo = PyMongo(app)
+
+def get_all_artists():
+    artists_collection = mongo.db.artists
+    artists_data = list(artists_collection.find({}, {'_id': 0}))
+    return {"artists": artists_data}
+
+def get_artist_by_id(artist_id):
+    artists_collection = mongo.db.artists
+    artist_data = artists_collection.find_one({'_id': ObjectId(artist_id)}, {'_id': 0})
+    return artist_data
+
+# Menghitung selisih popularity dan followers berdasarkan id
+def calculate_followers_popularity_difference(artist_id):
+    artists_collection = mongo.db.artists
+    artist_data = artists_collection.find_one({'_id': ObjectId(artist_id)}, {'_id': 0, 'followers_popularity': 1})
+
+    if 'followers_popularity' in artist_data:
+        followers_popularity_list = artist_data['followers_popularity']
+
+        # Sort the list by timestamp in ascending order
+        followers_popularity_list.sort(key=lambda x: x['timestamp'])
+
+        # Calculate the difference in followers and popularity based on timestamps
+        difference_list = []
+        for i in range(1, len(followers_popularity_list)):
+            diff_followers = followers_popularity_list[i]['followers'] - followers_popularity_list[i - 1]['followers']
+            diff_popularity = followers_popularity_list[i]['popularity'] - followers_popularity_list[i - 1]['popularity']
+
+            difference_list.append({
+                'timestamp': followers_popularity_list[i]['timestamp'],
+                'followers_difference': diff_followers,
+                'popularity_difference': diff_popularity
+            })
+
+        return {'followers_popularity_difference': difference_list}
+    else:
+        return {'message': 'Artist not found or no followers_popularity data available'}
+
+# Menghitung dan menampilkan semua selisih popularity dan followers
+def calculate_all_artists_followers_popularity_difference():
+    artists_collection = mongo.db.artists
+    all_artists_data = list(artists_collection.find({}, {'_id': 1, 'followers_popularity': 1}))
+
+    difference_list = []
+
+    for artist_data in all_artists_data:
+        artist_id = str(artist_data['_id'])
+        followers_popularity_list = artist_data.get('followers_popularity', [])
+
+        # Sort the list by timestamp in ascending order
+        followers_popularity_list.sort(key=lambda x: x['timestamp'])
+
+        # Calculate the difference in followers and popularity based on timestamps
+        for i in range(1, len(followers_popularity_list)):
+            diff_followers = followers_popularity_list[i]['followers'] - followers_popularity_list[i - 1]['followers']
+            diff_popularity = followers_popularity_list[i]['popularity'] - followers_popularity_list[i - 1]['popularity']
+
+            difference_list.append({
+                artist_id: {
+                    "followers_difference": diff_followers,
+                    "popularity_difference": diff_popularity,
+                }
+            })
+
+    return difference_list
+
+# Menghitung dan menampilkan semua selisih popularity followers
+def calculate_all_artists_followers_difference():
+    artists_collection = mongo.db.artists
+    all_artists_data = list(artists_collection.find({}, {'_id': 1, 'followers_popularity': 1}))
+
+    difference_list = []
+
+    for artist_data in all_artists_data:
+        artist_id = str(artist_data['_id'])
+        followers_popularity_list = artist_data.get('followers_popularity', [])
+
+        # Sort the list by timestamp in ascending order
+        followers_popularity_list.sort(key=lambda x: x['timestamp'])
+
+        # Calculate the difference in followers based on timestamps
+        for i in range(1, len(followers_popularity_list)):
+            diff_followers = followers_popularity_list[i]['followers'] - followers_popularity_list[i - 1]['followers']
+
+            difference_list.append({
+                artist_id: {
+                    "followers_difference": diff_followers,
+                }
+            })
+
+    return difference_list
+
+# Menghitung dan menampilkan semua selisih popularity artist
+def calculate_all_artists_popularity_difference():
+    artists_collection = mongo.db.artists
+    all_artists_data = list(artists_collection.find({}, {'_id': 1, 'followers_popularity': 1}))
+
+    difference_list = []
+
+    for artist_data in all_artists_data:
+        artist_id = str(artist_data['_id'])
+        followers_popularity_list = artist_data.get('followers_popularity', [])
+
+        # Sort the list by timestamp in ascending order
+        followers_popularity_list.sort(key=lambda x: x['timestamp'])
+
+        # Calculate the difference in popularity based on timestamps
+        for i in range(1, len(followers_popularity_list)):
+            diff_popularity = followers_popularity_list[i]['popularity'] - followers_popularity_list[i - 1]['popularity']
+
+            difference_list.append({
+                artist_id: {
+                    "popularity_difference": diff_popularity,
+                }
+            })
+
+    return difference_list
--- backend/app/routes/artist_route.py
+++ backend/app/routes/artist_route.py
@@ -1,4 +1,37 @@
 from flask import Blueprint, jsonify, request
-# from app.controllers.artist_controller import get_artist # sesuain methodnya nanti
+from app.controllers.artist_controller import get_all_artists, get_artist_by_id, calculate_followers_popularity_difference, calculate_all_artists_followers_popularity_difference, calculate_all_artists_followers_difference, calculate_all_artists_popularity_difference
 
 artist_route = Blueprint('artist_route', __name__, url_prefix='/api')
+
+@artist_route.route('/artist', methods=['GET'])
+def get_all_artists_route():
+    artists = get_all_artists()
+    return jsonify(artists)
+
+@artist_route.route('/artist/<artist_id>', methods=['GET'])
+def get_artist_by_id_route(artist_id):
+    artist = get_artist_by_id(artist_id)
+    if artist:
+        return jsonify(artist)
+    else:
+        return jsonify({'message': 'Artist not found'}), 404
+
+@artist_route.route('/artist/<artist_id>/followers-popularity-difference', methods=['GET'])
+def calculate_followers_popularity_difference_route(artist_id):
+    difference_data = calculate_followers_popularity_difference(artist_id)
+    return jsonify(difference_data)
+
+@artist_route.route('/artist/followers-popularity-difference/all', methods=['GET'])
+def calculate_all_artists_followers_popularity_difference_route():
+    difference_data = calculate_all_artists_followers_popularity_difference()
+    return jsonify(difference_data)
+
+@artist_route.route('/artist/followers-difference/all', methods=['GET'])
+def calculate_all_artists_followers_difference_route():
+    difference_data = calculate_all_artists_followers_difference()
+    return jsonify(difference_data)
+
+@artist_route.route('/artist/popularity-difference/all', methods=['GET'])
+def calculate_all_artists_popularity_difference_route():
+    difference_data = calculate_all_artists_popularity_difference()
+    return jsonify(difference_data)
